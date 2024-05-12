03fc8c72b52ec9e9b0957c30adca7ad78b5ea01a
Owais Hassan <owhassan@cisco.com>
2024-05-12 03:19:58 -0400

    made some major changes to the code

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
   imports: [],
   templateUrl: './dropdown.component.html',
-  styleUrl: './dropdown.component.scss'
+  styleUrl: './dropdown.component.scss',
 })
 export class DropdownComponent {
+  @ViewChild('dropdownbtn', { static: true })
+  dropdownbtn!: ElementRef<HTMLButtonElement>;
 
+  ngAfterViewInit() {
+    console.log(this.dropdownbtn.nativeElement);
+  }
+  toggleDropdownbtn(): void {
+    //this is used to toggle the dropdown visibility
+    const dropdownContent = this.dropdownbtn.nativeElement.nextElementSibling;
+    if (dropdownContent) {
+      dropdownContent.classList.toggle('show');
+    }
+  }
 }
